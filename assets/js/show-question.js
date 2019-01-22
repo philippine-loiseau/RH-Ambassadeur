0a4d4069851f91f8b40b95f68a4f9bdd75db0483
Philippine <philippine_l@live.fr>
2019-01-22 17:04:38 +0100

    Completed pages : search-ambassador and candidate account html

--- assets/js/show-question.js
+++ assets/js/show-question.js
@@ -1,19 +1,21 @@
 function Hide (addr) { document.getElementById(addr).style.visibility = "hidden";	}
 function Show (addr) { document.getElementById(addr).style.visibility = "visible";	}
 
+
 function toggle(anId)
 {
 	if (document.getElementById(anId).style.visibility == "hidden")	{	
+        document.getElementById("main-profile-candidate").classList.remove('main-profile-candidate');
+        document.getElementById("main-profile-candidate").classList.add('main-profile-candidate-visible');
         Show(anId);	
         
     }
     else {	
+        document.getElementById("main-profile-candidate").classList.add('main-profile-candidate');
         Hide(anId);	
-        document.getElementsByClassName("ask-question").style.background == "white";
     }
 }
 
 window.onload = function () { 
     Hide("view-form");	
-    document.getElementsByClassName("ask-question").style.background == "white";
 };
