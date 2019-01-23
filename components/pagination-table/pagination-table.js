e292ed70013e77738ba6f0f91422abaf73ddc591
Philippine <philippine_l@live.fr>
2019-01-23 17:02:01 +0100

    Finalize administrator account

--- components/pagination-table/pagination-table.js
+++ components/pagination-table/pagination-table.js
@@ -4,16 +4,67 @@ $(document).ready(function() {
         responsive: true,
         "language": {
             "paginate": {
-              "previous": '<img src="../../assets/img/icon/chevron-pagination-gauche.png">',
-              "next": '<img src="../../assets/img/icon/chevron-pagination-droite.png">'
+              "previous": '<img src="../assets/img/icon/chevron-pagination-gauche.png">',
+              "next": '<img src="../assets/img/icon/chevron-pagination-droite.png">'
             }
           },
           "columnDefs": [
             { "width": "10%", "targets": 0 },
-            { "width": "20%", "targets": 1 },
+            { "width": "15%", "targets": 1 },
             { "width": "10%", "targets": 2 },
-            { "width": "10%", "targets": 3 },
+            { "width": "20%", "targets": 3 },
             { "width": "5%", "targets": 4 },
+            { "width": "15%", "targets": 5 },
+            { "width": "10%", "targets": 6 },
+            { "width": "20%", "targets": 7 },
           ]  
     });
+} );
+
+
+$(document).ready(function() {
+  $('#ambassador').DataTable({
+      "searching": false,
+      responsive: true,
+      "language": {
+          "paginate": {
+            "previous": '<img src="../assets/img/icon/chevron-pagination-gauche.png">',
+            "next": '<img src="../assets/img/icon/chevron-pagination-droite.png">'
+          }
+        },
+        "columnDefs": [
+          { "width": "10%", "targets": 0 },
+          { "width": "10%", "targets": 1 },
+          { "width": "10%", "targets": 2 },
+          { "width": "5%", "targets": 3 },
+          { "width": "1%", "targets": 4 },
+          { "width": "5%", "targets": 5 },
+          { "width": "5%", "targets": 6 },
+          { "width": "1%", "targets": 7 },
+          { "width": "10%", "targets": 8 },
+        ]  
+  });
+} );
+
+
+$(document).ready(function() {
+  $('#candidate').DataTable({
+      "searching": false,
+      responsive: true,
+      "language": {
+          "paginate": {
+            "previous": '<img src="../assets/img/icon/chevron-pagination-gauche.png">',
+            "next": '<img src="../assets/img/icon/chevron-pagination-droite.png">'
+          }
+        },
+        "columnDefs": [
+          { "width": "10%", "targets": 0 },
+          { "width": "20%", "targets": 1 },
+          { "width": "20%", "targets": 2 },
+          { "width": "10%", "targets": 3 },
+          { "width": "10%", "targets": 4 },
+          { "width": "10%", "targets": 5 },
+          { "width": "10%", "targets": 6 }
+        ]  
+  });
 } );
